91a8ae1b9b668648ca047cdd954ed323367c0357
Rafael Pelle <rafapelle@gmail.com>
2019-08-03 15:41:07 -0300

    Updates the schema export

--- src/models/schema.ts
+++ src/models/schema.ts
@@ -1,4 +1,7 @@
 import { Account } from './account'
+import { Budget } from './budget'
+import { Reminder } from './reminder'
+import { Transaction } from './transaction'
 import { User } from './user'
 
 const Knex = require('knex')
@@ -8,4 +11,4 @@ const { Model } = require('objection')
 const knexConnection = Knex(connection)
 Model.knex(knexConnection)
 
-module.exports = { Account, User }
+module.exports = { Account, Budget, Reminder, Transaction, User }
