0226986f7a69122f8659ad9f571234108685cf7e
Rafael Pelle <rafapelle@gmail.com>
2019-08-03 14:23:42 -0300

    Refactoring

--- src/services/user/controllers.ts
+++ src/services/user/controllers.ts
@@ -1,9 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 const { User } = require('../../models/schema')
 
-
 export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
-	// select * from "users";
 	const users = await User.query()
 	res.status(200).send({
 		ok: true,
@@ -12,8 +10,6 @@ export const getAllUsers = async (req: Request, res: Response, next: NextFunctio
 }
 
 export const getUserByCPF = async ({ query }: Request, res: Response) => {
-	// SELECT user from database where cpf=...
-	// const result = await getUserByCPF(query.cpf)
 	const user = await User.query().where('cpf', query.cpf)
 	res.status(200).send({
 		ok: true,
